defc1a444fab444e8ee1c82b72342fa516e65ee8
Ryan Joseph <ryan@electricsheep.co>
2024-11-09 11:40:43 -0800

    Add SASL PLAIN support

--- src/ircpy/irc.py
+++ src/ircpy/irc.py
@@ -2,10 +2,14 @@ import asyncio
 import telnetlib3
 import re
 import os
+import base64
+import sys
+
 class Bot:
     def __init__(self,  **kwargs):
         self._events = {}
         self._args = kwargs
+        self.use_sasl_auth = os.environ.get("IRCPY_SASL_AUTH")
         global nickname
         global server
         global channel
@@ -49,22 +53,54 @@ class Bot:
         self.reader, self.writer = await telnetlib3.open_connection(
             server, port, encoding='utf-8'
         )
+
+        _og_writer = self.writer.write
+        def _writer_wrapper(*args, **kwargs):
+            if os.environ.get("IRCPY_DEBUG"):
+                sys.stdout.write(f"C>> {args[0]}")
+            return _og_writer(*args, **kwargs)
+        self.writer.write = _writer_wrapper
+
+        if self.use_sasl_auth:
+            self.writer.write("CAP REQ :sasl\r\n")
+
         self.writer.write(f"NICK {nickname}\r\n")
         self.writer.write(f"USER {nickname} 0 * :{nickname}\r\n")
+
         nickserv_password = os.environ.get("IRCPY_NICKSERV_PASSWORD")
         if nickserv_password:
-            self.writer.write(f"PRIVMSG NickServ :IDENTIFY {nickserv_password}\r\n")
-        self.writer.write(f"JOIN {channel}\r\n")
-        await self.callevent("ready", nickname, channel)
+            if self.use_sasl_auth:
+                self.writer.write("AUTHENTICATE PLAIN\r\n")
+            else:
+                self.writer.write(f"PRIVMSG NickServ :IDENTIFY {nickserv_password}\r\n")
+
+        if not self.use_sasl_auth:
+            self.writer.write(f"JOIN {channel}\r\n")
+            await self.callevent("ready", nickname, channel)
         while True:
             line = await self.reader.readline()
             if not line:
                 break
             await self.handle_line(line.strip())
     async def handle_line(self, line):
+        if os.environ.get("IRCPY_DEBUG"):
+            print(f"S>> {line}")
+
+        if self.use_sasl_auth and line.startswith("AUTHENTICATE +"):
+            auth_bytes = bytes(f"{nickname}\x00{nickname}\x00{os.environ.get('IRCPY_NICKSERV_PASSWORD')}", "utf-8")
+            auth_str = base64.b64encode(auth_bytes).decode("utf-8")
+            self.writer.write(f"AUTHENTICATE {auth_str}\r\n")
+            self.writer.write(f"CAP END\r\n")
+
+        if self.use_sasl_auth and line.find(":SASL authentication successful") != -1:
+            self.writer.write(f"PRIVMSG NickServ :IDENTIFY {os.environ.get('IRCPY_NICKSERV_PASSWORD')}\r\n")
+            self.writer.write(f"JOIN {channel}\r\n")
+            await self.callevent("ready", nickname, channel)
+
         if line.startswith("PING"):
             self.writer.write(f"PONG {line.split()[1]}\r\n")
-        elif "PRIVMSG" in line:
+
+        if "PRIVMSG" in line:
            splittedline = line.split('PRIVMSG')[0]
            msg = re.findall(f"(?<={channel} :).*$", line)
            if msg:
@@ -90,6 +126,3 @@ class Bot:
                    else:
                        arguments.append(arg)
                await self.callevent(cmd, arguments, user, channel, msg)
-        else:
-            if os.environ.get("IRCPY_DEBUG"):
-                print(line)
